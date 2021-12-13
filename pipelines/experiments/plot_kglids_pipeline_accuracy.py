8a318d248dc9965627ec1a31ee342b9267e81b94
Mossad Helali <mossadhelali@gmail.com>
2021-12-12 21:25:31 -0500

    Added script to download notebooks and datasets with Kaggle API
    - removed old script based on web scrapping.

---
+++ pipelines/experiments/plot_kglids_pipeline_accuracy.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy as np
+plt.style.use('ggplot')
+
+
+labels = ['Data Types', 'Parameter Names']
+kglids = [95, 87]
+graph4code = [64, 70]
+
+x = np.array([0, 0.6])  # the label locations
+width = 0.2  # the width of the bars
+
+fig, ax = plt.subplots(figsize=(4,4))
+rects1 = ax.bar(x -0.0125 - width/2, kglids, width, label='KGLiDS')
+rects2 = ax.bar(x +0.0125+ width/2, graph4code, width, label='GraphGen4Code')
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('Accuracy')
+ax.set_xticks(x, labels)
+ax.legend(loc='lower right')
+
+ax.bar_label(rects1, padding=1)
+ax.bar_label(rects2, padding=1)
+
+fig.tight_layout()
+
+plt.savefig('experiment_pipeline_accuracy.pdf')
+plt.show()
