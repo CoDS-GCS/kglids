efd7145d5772a56dcf11b02e299c730cd0bf5aea
Mossad Helali <mossadhelali@gmail.com>
2022-03-19 20:24:44 -0400

    Reduce the number of requests to elasticsearch per column pair
    - make a bulk request to get word embeddings for all required tokens instead of having multiple requests to elasticsearch. Hopefully this will resolve the issue of running kglids on the VM and help increase the performance overall by lowering the network bandwidth.

--- data_items/knowledge_graph/src/word_embedding/word_embeddings.py
+++ data_items/knowledge_graph/src/word_embedding/word_embeddings.py
@@ -37,18 +37,10 @@ class WordEmbeddings:
             print("unknowns")
             return -99
         else:
-            v1 = np.array(v1[0])
-            v2 = np.array(v2[0])
+            v1 = np.array(v1)
+            v2 = np.array(v2)
             sim = np.dot(v1, v2.T)
         return sim
-
-    def get_embedding_for_word(self, word):
-
-        try:
-            doc = self.es.get(index='word_embedding', id=word)
-            return doc['_source']['embedding']
-        except NotFoundError:
-            return None
 
     def get_distance_between_column_labels(self, l1, l2):
         l1_tokens = l1.split(' ')
@@ -60,29 +52,33 @@ class WordEmbeddings:
         intersection = set(l1_tokens).intersection(l2_tokens)
         # remove common token
         if len(l1_tokens) > 1 and len(l2_tokens) > 1:
-            for t in intersection:
-                l1_tokens.remove(t)
-                l2_tokens.remove(t)
+            l1_tokens = [i for i in l1_tokens if i not in intersection]
+            l2_tokens = [i for i in l2_tokens if i not in intersection]
+            
         if len(l1_tokens) >= 1 and len(l2_tokens) == 0 or len(l2_tokens) >= 1 and len(l1_tokens) == 0:
             l1_tokens = l1.split(' ')
             l2_tokens = l2.split(' ')
+        word_embeddings_for_tokens = self.get_embeddings_for_tokens(l1_tokens+l2_tokens)
+        
         combinations = list(itertools.product(l1_tokens, l2_tokens))
         distance = 0.0
         for v1, v2 in combinations:
-            emb1 = self.get_embedding_for_mwe(v1)
-            emb2 = self.get_embedding_for_mwe(v2)
-            if emb1[0] is None or emb2[0] is None:
+            emb1 = word_embeddings_for_tokens[v1]
+            emb2 = word_embeddings_for_tokens[v2]
+            if emb1 is None or emb2 is None:
                 return 0
+            
             d = self.semantic_distance(emb1, emb2)
             distance += d
         return (distance / len(combinations)) if combinations else 1
-
-    def get_embedding_for_mwe(self, mwe):
-        # TODO: [Refactor] remove or rename this method
-        words = mwe.strip().split()
-        mwe_vecs = []
-        for w in words:
-            mwe_vecs.append(self.get_embedding_for_word(w))
-
-        return mwe_vecs
-
+    
+    
+    def get_embeddings_for_tokens(self, tokens):
+        embeddings = {}
+        # TODO: [Implement] does it make sense to take only the first taken?
+        req_body = {'ids': [i.strip().split()[0] for i in tokens]}  
+        res = self.es.mget(index='word_embedding', body=req_body)
+        for i in range(len(tokens)):
+            embeddings[tokens[i]] = res['docs'][i]['_source']['embedding'] if res['docs'][i]['found'] else None
+        return embeddings
+    
