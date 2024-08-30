d55f320811c585cbc29e53e8cc349b994a95bb14
Mossad Helali <mossad@google.com>
2024-08-30 18:38:43 +0000

    Small fixes and improvements for data profiling
    - column and table ids are now in correct format: data_source/dataset_id/table_id/column_id
    - skipped unreadable columns when profiling

--- kg_governor/data_profiling/src/utils.py
+++ kg_governor/data_profiling/src/utils.py
@@ -6,11 +6,11 @@ def encode(entity):
 
 
 def generate_column_id(data_source: str, dataset_name: str, table_name: str, column_name: str):
-    return encode(data_source) + '/' + encode(dataset_name) + encode(table_name) + '/' + encode(column_name)
+    return encode(data_source) + '/' + encode(dataset_name) + '/' + encode(table_name) + '/' + encode(column_name)
 
 
 def generate_table_id(data_source: str, dataset_name: str, table_name: str):
-    return encode(data_source) + '/' + encode(dataset_name) + encode(table_name) 
+    return encode(data_source) + '/' + encode(dataset_name) + '/' + encode(table_name)
 
 
 def generate_dataset_id(data_source: str, dataset_name: str):
