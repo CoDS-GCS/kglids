df08f5cba955c9214d09b116075577ee36a5e786
Mossad Helali <mossadhelali@gmail.com>
2024-12-03 22:52:19 -0500

    Update .gitignore

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 # download fasttext embeddings
+# download glove embeddings
 # create kglids conda env
 # install requirements
 # install java for pyspark (make sure java is installed and JAVA_HOME is set) (https://www.digitalocean.com/community/tutorials/how-to-install-java-with-apt-on-ubuntu-22-04)
 # install graph db and configure default user and .graphdb-import
 # install postgress and pgvector and configure
+sudo apt install postgresql-16-pgvector
